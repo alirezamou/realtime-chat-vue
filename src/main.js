b958c8218fea18f59590662d363dc90174bb3e72
alirezamou <am808036@gmail.com>
2023-03-30 17:24:37 +0330

    add onAuthStateChanged function from firebase instead of method

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import store from "./store";
 import "bulma/css/bulma.css";
 
 import { FirebaseAuth } from "@/library/Database";
+import { onAuthStateChanged } from "firebase/auth";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
@@ -15,7 +16,7 @@ library.add(faIcons);
 
 let baseApp;
 
-FirebaseAuth.onAuthStateChanged(() => {
+onAuthStateChanged(FirebaseAuth, () => {
   if (!baseApp) {
     const baseApp = createApp(App);
     baseApp.component("fa-icon", FontAwesomeIcon);
