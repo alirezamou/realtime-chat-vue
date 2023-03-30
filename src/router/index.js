ad97578d9986164d7c4451bb379853272a9bc219
alirezamou <am808036@gmail.com>
2023-03-30 16:31:47 +0330

    add '/' path to Login comp

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,30 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+import Login from "@/components/Login.vue";
+
+import { FirebaseAuth } from "@/library/Database";
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      name: "Login",
+      path: "/",
+      component: Login,
+    },
+  ],
+});
+
+router.beforeEach((to, _from, next) => {
+  let auth = FirebaseAuth;
+
+  if (auth.currentUser === null && to.path !== "/login") {
+    next({
+      path: "/",
+    });
+  } else {
+    next();
+  }
 });
 
 export default router;
