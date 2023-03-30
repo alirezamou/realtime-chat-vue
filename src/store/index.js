af7221900ebc7103212a6a74be21ffdda0b9b630
alirezamou <am808036@gmail.com>
2023-03-30 16:37:23 +0330

    add 'loggedIn', 'uid', 'email' and 'username' into store state and mutators

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,22 @@
 import { createStore } from "vuex";
 
-const store = createStore({});
+const store = createStore({
+  state: {
+    loggedIn: false,
+    uid: "",
+    email: "",
+    username: "",
+  },
+  mutations: {
+    SET_LOGGEDIN: (state, { uid, email }) => {
+      state.loggedIn = true;
+      state.uid = uid;
+      state.email = email;
+    },
+    SET_USERNAME: (state, username) => {
+      state.username = username;
+    },
+  },
+});
 
 export default store;
