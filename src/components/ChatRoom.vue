2314d924977392d20c8b5ddfef4350f38bd61cbc
alirezamou <am808036@gmail.com>
2023-04-02 12:22:37 +0330

    add ChatRooms and ChatRoomUsers comp to ChatRoom comp and add the necessary methods

--- src/components/ChatRoom.vue
+++ src/components/ChatRoom.vue
@@ -1,49 +1,71 @@
 <template>
     <section class="container is-fluid">
-        <div class="chat-window">
-            <div class="chat-history">
-                <div class="chat-message" v-for="(message, i) in messages" :key="i">
-                    <div class="chat-message-meta">
-                        <p class="timestamp">{{ timeFormat(message.timestamp) }}</p>
-                        <p class="user-name">{{ message.name }}</p>
+        <div class="columns">
+            <div class="column is-2">
+                <rooms
+                  :selectedChatRoomSlug="selectedChatRoomSlug"
+                  @setActiveRoom="setActiveRoom"
+                />
+            </div>
+            <div class="column is-8">
+                <div class="chat-window">
+                    <div class="chat-history">
+                        <div class="chat-message" v-for="(message, i) in messages" :key="i">
+                            <div class="chat-message-meta">
+                                <p class="timestamp">{{ timeFormat(message.timestamp) }}</p>
+                                <p class="user-name">{{ message.name }}</p>
+                            </div>
+                            <p>{{ message.message }}</p>
+                        </div>
                     </div>
-                    <p>{{ message.message }}</p>
+                </div>
+                <div class="chat-actions">
+                    <form @submit.prevent="sendMessage">
+                        <div class="field has-addons">
+                            <p class="control is-expanded">
+                                <input class="input" type="text" v-model="chatMessage" placeholder="Your message" />
+                            </p>
+                            <button type="submit" class="button"><fa-icon icon="fa-solid fa-location-arrow" /></button>
+                        </div>
+                    </form>
                 </div>
             </div>
-        </div>
-        <div class="chat-actions">
-            <form @submit.prevent="sendMessage">
-                <div class="field has-addons">
-                    <p class="control is-expanded">
-                        <input class="input" type="text" v-model="chatMessage" placeholder="Your message" />
-                    </p>
-                    <button type="submit" class="button"><fa-icon icon="fa-solid fa-location-arrow" /></button>
-                </div>
-            </form>
+            <div class="column is-2">
+                <a @click="signout">
+                    <fa-icon icon="fa-solid fa-sign-out-alt" /> Signout
+                </a>
+                <hr />
+                <users :activeRoom="activeRoom" @kicked="kicked" />
+            </div>
         </div>
     </section>
 </template>
 
 <script>
 
-import { FirebaseDb } from "@/library/Database";
+import { FirebaseDb, FirebaseAuth } from "@/library/Database";
 import { ref, child, push, update, onChildAdded } from "firebase/database";
+import { signOut } from "firebase/auth";
 import moment from "moment";
+import ChatRooms from "@/components/ChatRooms.vue";
+import ChatRoomUsers from "@/components/ChatRoomUsers.vue";
 
 export default {
     name: "ChatRoom",
     created() {
         this.initializeRoom();
     },
+    components: {
+        rooms: ChatRooms,
+        users: ChatRoomUsers,
+    },
     data() {
         return {
             messages: [],
-            activeRoom: {
-                name: "General",
-                slug: "general"
-            },
+            activeRoom: {},
             chatMessage: "",
             unsubscribeRoom: null,
+            selectedChatRoomSlug: this.$route.params.slug,
         }
     },
     methods: {
@@ -62,9 +84,28 @@ export default {
             this.chatMessage = "";
 
         },
-        initializeRoom() {
+        initializeRoom(oldRoom = null) {
+            this.messages = [];
+
+            if(oldRoom !== null) {
+                this.unsubscribeRoom();
+                this.unsubscribeRoom = null;
+            }
+            
             const messagesRef = ref(FirebaseDb, "messages/" + this.activeRoom.slug);
-            onChildAdded(messagesRef, (data) => this.messages.push(data.val()));
+            this.unsubscribeRoom = onChildAdded(messagesRef, (data) => this.messages.push(data.val()));
+        },
+        kicked() {
+            this.$router.push("/chat/general")
+        },
+        signout() {
+            signOut(FirebaseAuth).then(() => {
+                this.$router.push("/");
+            })
+        },
+        setActiveRoom(room, oldRoom) {
+            this.activeRoom = room;
+            this.initializeRoom(oldRoom);
         },
         timeFormat(timestamp) {
             return moment.unix(timestamp).format("DD.MM.YYYY HH:mm:ss");
@@ -77,6 +118,10 @@ export default {
         uid() {
             return this.$store.state.uid;
         }
+    },
+    beforeRouteUpdate(to, _from, next) {
+        this.selectedChatRoomSlug = to.params.slug;
+        next();
     }
 }
 </script>
